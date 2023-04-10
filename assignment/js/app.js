1d305a3bab50b4f1af70fb97b07e263abfa7ecf4
Anh Bui <anhbhpd08263@fpt.edu.vn>
2023-04-10 13:51:03 +0700

    upload

--- assignment/js/app.js
+++ assignment/js/app.js
@@ -107,7 +107,7 @@ Validator.isPhoneNumber = function (selector) {
         selector: selector, 
         test: function (value) {
             var regexPhoneNumber = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g;
-                return regexPhoneNumber.test(value) ? undefined : 'Hmm ! Your phone number is not valid ? ';
+                return regexPhoneNumber.test(value) ? undefined : 'Hmm! Can\'t format phone number ? ';
         }
     };
 }
